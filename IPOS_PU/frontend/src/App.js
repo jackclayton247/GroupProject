760d67f3a16c1ec6f459874bd2180632204cc97c
trimm12 <tahrimrah@gmail.com>
2026-03-23 16:50:28 +0000

    added products page

--- IPOS_PU/frontend/src/App.js
+++ IPOS_PU/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import LoginPage from './pages/LoginPage';
+import ProductsPage from './pages/ProductsPage';
 import './App.css';
 
 function Home() {
@@ -16,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/products" element={<ProductsPage />} />
           </Routes>
         </main>
       </div>
---
+++ IPOS_PU/frontend/src/pages/ProductsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ProductsPage.css';
+
+function ProductsPage() {
+  return (
+    <div className="products-page">
+
+    </div>
+  );
+}
+
+export default ProductsPage;
