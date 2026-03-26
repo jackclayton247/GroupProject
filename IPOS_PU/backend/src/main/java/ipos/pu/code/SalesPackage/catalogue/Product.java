fb8c21d3fb296709169692fd173891bffdd3e98f
trimm12 <tahrimrah@gmail.com>
2026-03-26 23:36:44 +0000

    added product entities and repo for catalogue

---
+++ IPOS_PU/backend/src/main/java/ipos/pu/code/SalesPackage/catalogue/Product.java
@@ -0,0 +1,42 @@
+package ipos.pu.code.SalesPackage.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "item_id")
+    private String itemId;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "package_type")
+    private String packageType;
+
+    @Column(name = "units_in_pack")
+    private int unitsInPack;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "stock_quantity")
+    private int availability;
+
+    @Column(name = "is_active")
+    private int isActive;
+
+    public int getProductId() { return productId; }
+    public String getItemId() { return itemId; }
+    public String getDescription() { return description; }
+    public String getPackageType() { return packageType; }
+    public int getUnitsInPack() { return unitsInPack; }
+    public double getPrice() { return price; }
+    public int getAvailability() { return availability; }
+    public int getIsActive() { return isActive; }
+}
